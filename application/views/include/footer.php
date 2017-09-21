7c7d2cf10a07689dd7b239039bbea2a1b7ae23b4
DaleAngeles <dale.angeles09@gmail.com>
2017-09-21 15:39:20 +0800

    homepage

---
+++ application/views/include/footer.php
@@ -0,0 +1,13 @@
+    <div id="bg2"></div>
+    <footer>
+        <div class="row">
+            <div class="col-md-6 col-sm-6 footer-navigation">
+                <h3 class="text-nowrap text-right"><a href="#"><img src="assets/img/logo.png" id="logo2">noname</a></h3>
+                <p class="text-right company-name">UNDEFINED COMPANY © 2017 </p>
+            </div>
+            <div class="clearfix visible-sm-block"></div>
+            <div class="col-md-3 footer-about">
+                <div class="social-links social-icons"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-github"></i></a></div>
+            </div>
+        </div>
+    </footer>
---
+++ application/views/include/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title><?php echo $title; ?></title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<link href="<?php echo base_url('bootstrap/css/bootstrap.css'); ?>" rel="stylesheet" />
+<link href="<?php echo base_url('bootstrap/css/bootstrap-theme.css'); ?>" rel="stylesheet" />
+<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+<link href="assets/fonts/font-awesome.min.css" rel="stylesheet" />
+<link href="assets/css/styles.css" rel="stylesheet" />
+<link href="assets/css/Pretty-Footer.css" rel="stylesheet" />
+
+<script src="<?php echo base_url('bootstrap/js/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>
+
+</head>
+<body>

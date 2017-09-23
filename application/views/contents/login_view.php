348cfa153eb64b3d11726bbe5f797ff5cd2f5ab9
Leonardo Pajuyo <pajuyo.leo@gmail.com>
2017-09-24 00:37:12 +0800

    controller functions for views, href and src fixes

--- application/views/contents/login_view.php
+++ application/views/contents/login_view.php
@@ -16,7 +16,7 @@
         <nav class="navbar navbar-inverse" id="nav">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand navbar-link" href="#"><img src="assets/img/logo.png" id="logo"></a>
+                    <a class="navbar-brand navbar-link" href="#"><img src="<?php echo base_url('assets/img/logo.png'); ?>" id="logo"></a>
                     <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
                 </div>
                 <div class="collapse navbar-collapse" id="navcol-1">
@@ -35,8 +35,8 @@
                 <button class="btn btn-default btn-block btn-lg btn-signin" type="submit">Sign in</button>
             </form><a href="#" class="forgot-password">Forgot your password?</a></div>
     </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+    <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>
 </body>
 
 </html>

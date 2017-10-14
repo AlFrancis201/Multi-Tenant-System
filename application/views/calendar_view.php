7610eeafcfc51b4118c19591a9d1f051a8b9a4b6
Leonardo Pajuyo <pajuyo.leo@gmail.com>
2017-10-14 20:05:55 +0800

    added calendar view(WIP)

---
+++ application/views/calendar_view.php
@@ -0,0 +1,17 @@
+<div class="main">
+    <div id="calendar">
+    </div>
+</div>
+ <script>
+    $(document).ready(function() {
+
+        // page is now ready, initialize the calendar...
+
+        $('#calendar').fullCalendar({
+            // put your options and callbacks here
+        })
+
+    });
+    </script>
+</body>
+</html>

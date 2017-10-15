482587b4ae48ffcdfeac89fe51e9a658fcac5a64
Leonardo Pajuyo <pajuyo.leo@gmail.com>
2017-10-15 18:53:01 +0800

    added services checkboxes to add staff form

--- application/views/contents/add_staff.php
+++ application/views/contents/add_staff.php
@@ -15,112 +15,6 @@
                 <div class="col-md-7">
                     <div class="checkbox">
                         <label>
-                        <input type="checkbox" name="day[]" value="sunday">Sunday
-                        </label>
-                        <select class="form-control" name="start_time[]" data-day="sunday" disabled>
-                            <option value="0.0">12:00 am (midnight)</option>
-                            <option value="0.30">12:30 am </option>
-                            <option value="1.0">1:00 am </option>
-                            <option value="1.30">1:30 am </option>
-                            <option value="2.0">2:00 am </option>
-                            <option value="2.30">2:30 am </option>
-                            <option value="3.0">3:00 am </option>
-                            <option value="3.30">3:30 am </option>
-                            <option value="4.0">4:00 am </option>
-                            <option value="4.30">4:30 am </option>
-                            <option value="5.0">5:00 am </option>
-                            <option value="5.30">5:30 am </option>
-                            <option value="6.0">6:00 am </option>
-                            <option value="6.30">6:30 am </option>
-                            <option value="7.0">7:00 am </option>
-                            <option value="7.30">7:30 am </option>
-                            <option value="8.0" selected>8:00 am </option>
-                            <option value="8.30">8:30 am </option>
-                            <option value="9.0">9:00 am </option>
-                            <option value="9.30">9:30 am </option>
-                            <option value="10.0">10:00 am </option>
-                            <option value="10.30">10:30 am </option>
-                            <option value="11.0">11:00 am </option>
-                            <option value="11.30">11:30 am </option>
-                            <option value="12.0">12:00 pm (noon)</option>
-                            <option value="12.30">12:30 pm </option>
-                            <option value="13.0">1:00 pm </option>
-                            <option value="13.30">1:30 pm </option>
-                            <option value="14.0">2:00 pm </option>
-                            <option value="14.30">2:30 pm </option>
-                            <option value="15.0">3:00 pm </option>
-                            <option value="15.30">3:30 pm </option>
-                            <option value="16.0">4:00 pm </option>
-                            <option value="16.30">4:30 pm </option>
-                            <option value="17.0">5:00 pm </option>
-                            <option value="17.30">5:30 pm </option>
-                            <option value="18.0">6:00 pm </option>
-                            <option value="18.30">6:30 pm </option>
-                            <option value="19.0">7:00 pm </option>
-                            <option value="19.30">7:30 pm </option>
-                            <option value="20.0">8:00 pm </option>
-                            <option value="20.30">8:30 pm </option>
-                            <option value="21.0">9:00 pm </option>
-                            <option value="21.30">9:30 pm </option>
-                            <option value="22.0">10:00 pm </option>
-                            <option value="22.30">10:30 pm </option>
-                            <option value="23.0">11:00 pm </option>
-                            <option value="23.30">11:30 pm </option>
-                        </select>
-                        to
-                        <select class="form-control" name="end_time[]" data-day="sunday" disabled>
-                            <option value="0.0">12:00 am (midnight)</option>
-                            <option value="0.30">12:30 am </option>
-                            <option value="1.0">1:00 am </option>
-                            <option value="1.30">1:30 am </option>
-                            <option value="2.0">2:00 am </option>
-                            <option value="2.30">2:30 am </option>
-                            <option value="3.0">3:00 am </option>
-                            <option value="3.30">3:30 am </option>
-                            <option value="4.0">4:00 am </option>
-                            <option value="4.30">4:30 am </option>
-                            <option value="5.0">5:00 am </option>
-                            <option value="5.30">5:30 am </option>
-                            <option value="6.0">6:00 am </option>
-                            <option value="6.30">6:30 am </option>
-                            <option value="7.0">7:00 am </option>
-                            <option value="7.30">7:30 am </option>
-                            <option value="8.0">8:00 am </option>
-                            <option value="8.30">8:30 am </option>
-                            <option value="9.0">9:00 am </option>
-                            <option value="9.30">9:30 am </option>
-                            <option value="10.0">10:00 am </option>
-                            <option value="10.30">10:30 am </option>
-                            <option value="11.0">11:00 am </option>
-                            <option value="11.30">11:30 am </option>
-                            <option value="12.0">12:00 pm (noon)</option>
-                            <option value="12.30">12:30 pm </option>
-                            <option value="13.0">1:00 pm </option>
-                            <option value="13.30">1:30 pm </option>
-                            <option value="14.0">2:00 pm </option>
-                            <option value="14.30">2:30 pm </option>
-                            <option value="15.0">3:00 pm </option>
-                            <option value="15.30">3:30 pm </option>
-                            <option value="16.0">4:00 pm </option>
-                            <option value="16.30">4:30 pm </option>
-                            <option value="17.0" selected>5:00 pm </option>
-                            <option value="17.30">5:30 pm </option>
-                            <option value="18.0">6:00 pm </option>
-                            <option value="18.30">6:30 pm </option>
-                            <option value="19.0">7:00 pm </option>
-                            <option value="19.30">7:30 pm </option>
-                            <option value="20.0">8:00 pm </option>
-                            <option value="20.30">8:30 pm </option>
-                            <option value="21.0">9:00 pm </option>
-                            <option value="21.30">9:30 pm </option>
-                            <option value="22.0">10:00 pm </option>
-                            <option value="22.30">10:30 pm </option>
-                            <option value="23.0">11:00 pm </option>
-                            <option value="23.30">11:30 pm </option>
-                        </select>
-                    </div>
-                    <div class="checkbox">
-                        <label>
                         <input type="checkbox" name="day[]" value="monday">Monday
                         </label>
                         <select class="form-control" name="start_time[]" data-day="monday" disabled>
@@ -755,6 +649,128 @@
                             <option value="23.30">11:30 pm </option>
                         </select>
                     </div>
+                    <div class="checkbox">
+                        <label>
+                        <input type="checkbox" name="day[]" value="sunday">Sunday
+                        </label>
+                        <select class="form-control" name="start_time[]" data-day="sunday" disabled>
+                            <option value="0.0">12:00 am (midnight)</option>
+                            <option value="0.30">12:30 am </option>
+                            <option value="1.0">1:00 am </option>
+                            <option value="1.30">1:30 am </option>
+                            <option value="2.0">2:00 am </option>
+                            <option value="2.30">2:30 am </option>
+                            <option value="3.0">3:00 am </option>
+                            <option value="3.30">3:30 am </option>
+                            <option value="4.0">4:00 am </option>
+                            <option value="4.30">4:30 am </option>
+                            <option value="5.0">5:00 am </option>
+                            <option value="5.30">5:30 am </option>
+                            <option value="6.0">6:00 am </option>
+                            <option value="6.30">6:30 am </option>
+                            <option value="7.0">7:00 am </option>
+                            <option value="7.30">7:30 am </option>
+                            <option value="8.0" selected>8:00 am </option>
+                            <option value="8.30">8:30 am </option>
+                            <option value="9.0">9:00 am </option>
+                            <option value="9.30">9:30 am </option>
+                            <option value="10.0">10:00 am </option>
+                            <option value="10.30">10:30 am </option>
+                            <option value="11.0">11:00 am </option>
+                            <option value="11.30">11:30 am </option>
+                            <option value="12.0">12:00 pm (noon)</option>
+                            <option value="12.30">12:30 pm </option>
+                            <option value="13.0">1:00 pm </option>
+                            <option value="13.30">1:30 pm </option>
+                            <option value="14.0">2:00 pm </option>
+                            <option value="14.30">2:30 pm </option>
+                            <option value="15.0">3:00 pm </option>
+                            <option value="15.30">3:30 pm </option>
+                            <option value="16.0">4:00 pm </option>
+                            <option value="16.30">4:30 pm </option>
+                            <option value="17.0">5:00 pm </option>
+                            <option value="17.30">5:30 pm </option>
+                            <option value="18.0">6:00 pm </option>
+                            <option value="18.30">6:30 pm </option>
+                            <option value="19.0">7:00 pm </option>
+                            <option value="19.30">7:30 pm </option>
+                            <option value="20.0">8:00 pm </option>
+                            <option value="20.30">8:30 pm </option>
+                            <option value="21.0">9:00 pm </option>
+                            <option value="21.30">9:30 pm </option>
+                            <option value="22.0">10:00 pm </option>
+                            <option value="22.30">10:30 pm </option>
+                            <option value="23.0">11:00 pm </option>
+                            <option value="23.30">11:30 pm </option>
+                        </select>
+                        to
+                        <select class="form-control" name="end_time[]" data-day="sunday" disabled>
+                            <option value="0.0">12:00 am (midnight)</option>
+                            <option value="0.30">12:30 am </option>
+                            <option value="1.0">1:00 am </option>
+                            <option value="1.30">1:30 am </option>
+                            <option value="2.0">2:00 am </option>
+                            <option value="2.30">2:30 am </option>
+                            <option value="3.0">3:00 am </option>
+                            <option value="3.30">3:30 am </option>
+                            <option value="4.0">4:00 am </option>
+                            <option value="4.30">4:30 am </option>
+                            <option value="5.0">5:00 am </option>
+                            <option value="5.30">5:30 am </option>
+                            <option value="6.0">6:00 am </option>
+                            <option value="6.30">6:30 am </option>
+                            <option value="7.0">7:00 am </option>
+                            <option value="7.30">7:30 am </option>
+                            <option value="8.0">8:00 am </option>
+                            <option value="8.30">8:30 am </option>
+                            <option value="9.0">9:00 am </option>
+                            <option value="9.30">9:30 am </option>
+                            <option value="10.0">10:00 am </option>
+                            <option value="10.30">10:30 am </option>
+                            <option value="11.0">11:00 am </option>
+                            <option value="11.30">11:30 am </option>
+                            <option value="12.0">12:00 pm (noon)</option>
+                            <option value="12.30">12:30 pm </option>
+                            <option value="13.0">1:00 pm </option>
+                            <option value="13.30">1:30 pm </option>
+                            <option value="14.0">2:00 pm </option>
+                            <option value="14.30">2:30 pm </option>
+                            <option value="15.0">3:00 pm </option>
+                            <option value="15.30">3:30 pm </option>
+                            <option value="16.0">4:00 pm </option>
+                            <option value="16.30">4:30 pm </option>
+                            <option value="17.0" selected>5:00 pm </option>
+                            <option value="17.30">5:30 pm </option>
+                            <option value="18.0">6:00 pm </option>
+                            <option value="18.30">6:30 pm </option>
+                            <option value="19.0">7:00 pm </option>
+                            <option value="19.30">7:30 pm </option>
+                            <option value="20.0">8:00 pm </option>
+                            <option value="20.30">8:30 pm </option>
+                            <option value="21.0">9:00 pm </option>
+                            <option value="21.30">9:30 pm </option>
+                            <option value="22.0">10:00 pm </option>
+                            <option value="22.30">10:30 pm </option>
+                            <option value="23.0">11:00 pm </option>
+                            <option value="23.30">11:30 pm </option>
+                        </select>
+                    </div>
+                </div>
+                
+                <h1 class="text-info">Services Provided</h1>
+                <div class="col-md-7">
+                    <div class="checkbox">
+                        <label><input id="all_service" type="checkbox" />All Services</label>
+                    </div>
+                <?php
+                    if($service != false){
+                        foreach($service as $s){
+                            echo '<div class="checkbox">';
+                            echo '<label><input type="checkbox" name="service[]" value="'.$s['service_id'].'" />'.$s['service_name'].'</label>';
+                            echo '</div>';
+                        }
+                    }
+                ?>
                 </div>
                 
                 <div class="col-md-7 inputColumn">
@@ -767,7 +783,7 @@
 
 <script>
     $(document).ready(function(){
-        $('input[type="checkbox"]').click(function(event){
+        $('input[name="day[]"]').click(function(event){
             var day = $(event.target).val();
             if($(this).prop('checked')==true){
                 $('select[data-day="'+day+'"]').prop('disabled',false);
@@ -777,6 +793,29 @@
             }
         });
     });
+</script>
+<script>
+$(document).ready(function(){
+    $('#all_service').click(function(){
+        if($(this).prop('checked') == true){
+            $('input[name="service[]"]').prop('checked',true);
+        }
+        else{
+            $('input[name="service[]"]').prop('checked',false);
+        }
+    });
+});
+</script>
+<script>
+$(document).ready(function(){
+    $('input[name="service[]"]').click(function(){
+        if($('input[name="service[]"]:checked').length == $('input[name="service[]"]').length){
+            $('input[type="checkbox"').prop('checked',true);
+        }
+        else
+            $('#all_service').prop('checked',false);
+    });
+});
 </script>
 
 </body>

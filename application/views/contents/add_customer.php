1a4e5388ba9c87c2f402f2cdc40df37688aed5f8
haroldnicole <nclguiao@gmail.com>
2017-10-16 12:39:38 +0800

    fixed add customer form

---
+++ application/views/contents/add_customer.php
@@ -0,0 +1,67 @@
+<div class="main">
+	<div class="container">
+        <?php echo validation_errors(); ?>
+		<form class="form-horizontal customerForm" action="<?php echo base_url('mts/add_customer'); ?>" method="post">	
+				<div class="form-group ">
+					<span style="font-size:80px;" class="glyphicon glyphicon-user col-md-1"> </span>
+	      		</div>
+				<div class="form-group">
+					<label class="control-label col-md-1" for="cname">Name:</label>
+	      			<div class="col-md-4">
+	        			<input type="cname" class="form-control" id="cname" placeholder="Name" name="cname">
+	        		</div>
+	      		</div>		
+				<div class="form-group">
+					<label class="control-label col-md-1" for="email">Email:</label>
+	      			<div class="col-md-4">
+	        			<input type="email" class="form-control" id="email" placeholder="Email" name="email">
+	        		</div>
+	      		</div>		
+				<div class="form-group">
+					<label class="control-label col-md-1" for="mobile">Mobile:</label>
+	      			<div class="col-md-4">
+	        			<input type="text" class="form-control" id="mobile" placeholder="Phone" name="mobile">
+	        		</div>
+	      		</div>	
+				<div class="form-group">
+					<label class="control-label col-md-1" for="office">Office:</label>
+	      			<div class="col-md-4">
+	        			<input type="text" class="form-control" id="office" placeholder="Phone" name="office">
+	        		</div>
+	      		</div>					
+	      		<div class="form-group">
+					<label class="control-label col-md-1" for="home">Home:</label>
+	      			<div class="col-md-4">
+	        			<input type="text" class="form-control" id="home" placeholder="Phone" name="home">
+	        		</div>
+	      		</div>		      		
+	      		<div class="form-group">
+					<label class="control-label col-md-1" for="address">Address:</label>
+	      			<div class="col-md-4">
+	        			<input type="text" class="form-control" id="" placeholder="Address" name="address">
+	        		</div>
+	      		</div>	
+				<div class="form-group">
+					<label class="control-label col-md-1" for="city">City:</label>
+	      			<div class="col-md-4">
+	        			<input type="text" class="form-control" id="city" placeholder="City" name="city">
+	        		</div>
+	      		</div>
+				<div class="form-group">
+					<label class="control-label col-md-1" for="state">State:</label>
+	      			<div class="col-md-4">
+	        			<input type="text" class="form-control" id="state" placeholder="State" name="state">
+	        		</div>
+	      		</div>
+				<div class="form-group">
+					<label class="control-label col-md-1" for="zip">Zip:</label>
+	      			<div class="col-md-4">
+	        			<input type="text" class="form-control" id="zip" placeholder="Zip" name="zip">
+	        		</div>
+	      		</div>
+
+                <button class="form-control">Add</button>
+		</form>
+</div>
+</body>
+</html>

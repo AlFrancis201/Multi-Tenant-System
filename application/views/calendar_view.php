e60a36115e54a8f7db42c06a685b3ea5304e053e
Al Francis Orias <uchihafrancis.orias@gmail.com>
2017-10-20 14:34:35 +0800

    fixed active sidebar link, fixed some views

--- application/views/calendar_view.php
+++ application/views/calendar_view.php
@@ -1,79 +1,82 @@
-<div class="main">
-    <div>
-        <button class="btn btn-default" type="button" data-toggle="modal" data-target="#appointment">Make Appointment</button>
-    </div>
-    <div id="calendar">
-     
-    </div>
-    
-    <div class="modal fade" role="dialog" tabindex="1" id="appointment">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                    <h4 class="modal-title">Appointment </h4></div>
-                <div class="modal-body">
-                    <div>
-                        <ul class="nav nav-tabs appntTab"> <!--tab links -->
-                            <li class="active"><a href="#tab-1" role="tab" data-toggle="tab">DETAILS </a></li>
-                            <li><a href="#tab-2" role="tab" data-toggle="tab">CUSTOMER </a></li>
-                        </ul>
-                        
-                        <form class="form-horizontal appointmentForm" action="<?php echo base_url('mts/add_appointment'); ?>" method="post"> 
-                            <div class="tab-content">
-                                <div class="tab-pane active" role="tabpanel" id="tab-1">     
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3" for="">Provider:</label>
-                                        <div class="col-md-9">
-                                            <select name="provider">
-                                                <option disabled selected hidden>Select Provider</option>
-                                                <?php
-                                                if($provider != false){
-                                                    foreach($provider as $p){
-                                                        echo '<option value="'.$p['staff_id'].'">'.$p['first_name'].' '.$p['last_name'].'</option>';
+<div class="content-wrapper">
+    <section class="content">
+        <div>
+            <button class="btn btn-default" type="button" data-toggle="modal" data-target="#appointment">Make Appointment</button>
+        </div>
+        <div id="calendar">
+         
+        </div>
+        
+        <div class="modal fade" role="dialog" tabindex="1" id="appointment">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                        <h4 class="modal-title">Appointment </h4></div>
+                    <div class="modal-body">
+                        <div>
+                            <ul class="nav nav-tabs appntTab"> <!--tab links -->
+                                <li class="active"><a href="#tab-1" role="tab" data-toggle="tab">DETAILS </a></li>
+                                <li><a href="#tab-2" role="tab" data-toggle="tab">CUSTOMER </a></li>
+                            </ul>
+                            
+                            <form class="form-horizontal appointmentForm" action="<?php echo base_url('mts/add_appointment'); ?>" method="post"> 
+                                <div class="tab-content">
+                                    <div class="tab-pane active" role="tabpanel" id="tab-1">     
+                                        <div class="form-group">
+                                            <label class="control-label col-md-3" for="">Provider:</label>
+                                            <div class="col-md-9">
+                                                <select name="provider">
+                                                    <option disabled selected hidden>Select Provider</option>
+                                                    <?php
+                                                    if($provider != false){
+                                                        foreach($provider as $p){
+                                                            echo '<option value="'.$p['staff_id'].'">'.$p['first_name'].' '.$p['last_name'].'</option>';
+                                                        }
                                                     }
-                                                }
-                                                ?>
-                                            </select>
+                                                    ?>
+                                                </select>
+                                            </div>
+                                        </div>      
+                                        <div class="form-group">
+                                            <label class="control-label col-md-3" for="">Service:</label>
+                                            <div class="col-md-9">
+                                                <select name="service">
+                                                </select>
+                                            </div>
+                                        </div>  
+                                        <div class="form-group">
+                                            <label class="control-label col-md-3" for="">Day/Time:</label>
+                                            <div class="col-md-9">
+                                                <input type="text" class="form-control datepicker" id="" placeholder="" name="date">
+                                                <input type="time" class="form-control" name="time">
+                                            </div>
+                                        </div>                  
+                                        <!--<div class="form-group">
+                                            <label class="control-label col-md-3" for="">Notes:</label>
+                                            <div class="col-md-9">
+                                                <input type="text" class="form-control" id="" placeholder="Notes for the Customer">
+                                            </div>
+                                        </div>-->                  
+                                        <!--<button class="btn btn-primary btn-sm" id="appntcont" type="button">Continue </button>  -->
+                                    </div>
+                                    <div class="tab-pane" role="tabpanel" id="tab-2">
+                                        <div id="searchTab">
+                                            <input type="text" id="myInput" placeholder="Search Customer"><span class="pull-right glyphicon glyphicon-search">
+                                            </span>
+                                            <button id="new-customer" type="button" class="btn new-cust">New Customer</button>
+                                            <!--<button class="btn btn-primary btn-sm" id="appntcont" type="button">Save Appointment </button>-->
                                         </div>
-                                    </div>      
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3" for="">Service:</label>
-                                        <div class="col-md-9">
-                                            <select name="service">
-                                            </select>
-                                        </div>
-                                    </div>  
-                                    <div class="form-group">
-                                        <label class="control-label col-md-3" for="">Day/Time:</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control datepicker" id="" placeholder="" name="date">
-                                            <input type="time" class="form-control" name="time">
-                                        </div>
-                                    </div>                  
-                                    <!--<div class="form-group">
-                                        <label class="control-label col-md-3" for="">Notes:</label>
-                                        <div class="col-md-9">
-                                            <input type="text" class="form-control" id="" placeholder="Notes for the Customer">
-                                        </div>
-                                    </div>-->                  
-                                    <!--<button class="btn btn-primary btn-sm" id="appntcont" type="button">Continue </button>  -->
-                                </div>
-                                <div class="tab-pane" role="tabpanel" id="tab-2">
-                                    <div id="searchTab">
-                                        <input type="text" id="myInput" placeholder="Search Customer"><span class="pull-right glyphicon glyphicon-search">
-                                        </span>
-                                        <button id="new-customer" type="button" class="btn new-cust">New Customer</button>
-                                        <!--<button class="btn btn-primary btn-sm" id="appntcont" type="button">Save Appointment </button>-->
                                     </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+</div>
 <script>
 $(document).ready(function() {
 

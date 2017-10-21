98a6b5eba7a5c536a7bc4bcb50a9cc03599b3384
haroldnicole <nclguiao@gmail.com>
2017-10-21 13:40:55 -0700

    updates login/register

--- application/views/contents/login_view.php
+++ application/views/contents/login_view.php
@@ -10,32 +10,44 @@
     <link rel="stylesheet" href="<?php echo base_url('assets/css/styles2.css'); ?>">
     <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
     <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>    
+	
+	<nav class="nav navbar navbar-default1 navbar-fixed-top">
+  <div class="container-fluid bg">
+    <div class="navbar-header">
+       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>       
+      <a class="navbar-brand">
+        <a href="<?php echo base_url('home'); ?>"> <img src="<?php echo base_url('assets/img/Ico.svg')?>" id="logo1" class="logo"/>
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">    
+    <ul class="nav navbar-nav pull-right">
+      <li><a href="<?php echo base_url('registration'); ?>">CREATE AN ACCOUNT</a></li>
+    </ul>
+    </div>
+  </div>
+</nav>
+	
 </head>
 
 <body>
-    <div class="container-fluid bgsignin">
-        <nav class="navbar navbar-inverse navbar-fixed-top" id="nav">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand navbar-link" href="#"><img src="<?php echo base_url('assets/img/logo.png'); ?>" id="logo"></a>
-                    <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-                </div>
-                <div class="collapse navbar-collapse" id="navcol-1">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li role="presentation"><a href="<?php echo base_url('registration'); ?>">CREATE AN ACCOUNT</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="login-card">
-            <p class="profile-name-card">SIGN IN</p>
+    
+        <div class="container ">
+        <div class="login-card1" >
+			
+            <p class="profile-name-card1">SIGN IN</p>
             <?php echo validation_errors(); ?>
-            <form class="form-signin" action="<?php echo base_url('login')?>" method='post'><span class="reauth-email"> </span>
+            <form class="form-signin1" action="<?php echo base_url('login')?>" method='post'><span class="reauth-email1"> </span>
                 <input class="form-control" type="email" required="" placeholder="Email address" autofocus="" id="inputEmail" name="email">
                 <input class="form-control" type="password" required="" placeholder="Password" id="inputPassword" name="password">
-                <button class="btn btn-default btn-block btn-lg btn-signin" type="submit">Sign in</button>
-            </form><a href="#" class="forgot-password">Forgot your password?</a></div>
-    </div>
+                <button class="btn btn-default1 btn-block1 btn-lg btn-signin1" type="submit">Sign in</button>
+            </form><a href="#" class="forgot-password1">Forgot your password?</a>
+			</div>
+			</div>
+   
 </body>
 
 </html>
--- application/views/contents/register_view.php
+++ application/views/contents/register_view.php
@@ -10,12 +10,46 @@
     <link rel="stylesheet" href="<?php echo base_url('assets/css/styles2.css'); ?>">
     <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
     <script src="<?php echo base_url('bootstrap/js/bootstrap.min.js'); ?>"></script>    
+	
+	
+<nav class="nav navbar navbar-default1 navbar-fixed-top">
+  <div class="container-fluid bg">
+    <div class="navbar-header">
+       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>       
+      <a class="navbar-brand">
+        <a href="<?php echo base_url('home'); ?>"> <img src="<?php echo base_url('assets/img/Ico.svg')?>" id="logo1" class="logo"/>
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">    
+    <ul class="nav navbar-nav pull-right">
+      <li><a href="<?php echo base_url('login'); ?>">SIGN IN</a></li>
+    </ul>
+    </div>
+  </div>
+</nav>
+
 </head>
 
 <body>
-   <div class="container-fluid bgsignin">
-       
-            <div class="login-card">
+
+<div class="container-fluid bgsignin">
+			
+		<div class="container">
+		<div class="row">
+			<div class="col-md-7 margin-top left">
+			<div class="align-center">
+			<h2>Take it all with you</h2>
+            <h4>Switch between devices, and pick up wherever you left off.</h4>
+			<img src="<?php echo base_url('assets/img/device1.jpg')?>" id="device" class="device"/>
+            </div>
+            </div>
+			
+			<div class="col-md-5">
+			<div class="login-card">
                 <p class="profile-name-card">CREATE AN ACCOUNT</p>
               <?php echo validation_errors(); ?>
                 <?php echo form_open('registration','class="form-signin"'); ?> 
@@ -25,10 +59,14 @@
                     <input class="form-control" type="text" placeholder="Last name" id="inputName" name="lname">
                     <input class="form-control" type="email" name="email" required="" placeholder="Email address" id="inputEmail" >
                     <input class="form-control" type="password" name="password" required="" placeholder="Password" id="inputPassword" name="password">
+					<input type="radio" name="gender" value="male"> Male<br>
+					<input type="radio" name="gender" value="female"> Female<br>
+					<input type="radio" name="gender" value="other"> Other
                     <div class="checkbox"></div>
                     <input class="btn btn-default btn-block btn-lg btn-signin" type="submit" value="Register"> <!--Register </button>-->
                  </form>
-            
+            </div>
+			</div>
         </div>
         </div>
     

63a0943af5a9dbef463f871895522cd806d006ad
DaleAngeles <dale.angeles09@gmail.com>
2017-10-23 05:49:29 +0800

    homepage

--- application/views/contents/Homepage.php
+++ application/views/contents/Homepage.php
@@ -13,34 +13,7 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">    
     <ul class="nav navbar-nav pull-right">
-     <button type="button" onclick="document.getElementById('id01').style.display='block'" style="width:auto;" class="button button1 top1">SIGN IN</button>
-
-	<div id="id01" class="modal">
-  
-  <form class="modal-content animate" action="<?php echo base_url('login')?>" method='post'>
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="<?php echo base_url('assets/img/signin.png')?>" alt="Avatar" class="avatar">
-    </div>
-
-    <div class="container">
-      <label><b>Username</b></label></br>
-      <input type="text" placeholder="Enter Username" name="email" required></br>
-
-      <label><b>Password</b></label></br>
-      <input type="password" placeholder="Enter Password" name="password" required></br>
-        
-      <button type="submit">Login</button>
-    </div>
-
-	
-    <div class="container" >
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      
-    </div>
-  </form>
-</div>
-
+      <li><a href="<?php echo base_url('login'); ?>">SIGN IN</a></li>
       <li><a href="<?php echo base_url('registration');?>">CREATE AN ACCOUNT</a></li>
     </ul>
     </div>
@@ -48,59 +21,55 @@
 </nav>
 
 <div id="wrapper">
-	<div class="section">
+	<div class="full-page section1">
 		<div class="container1">
-            <div class="row">
+            <div class="row">		
                 <div class="col-md-1">
-				<img id="bg1" src="<?php echo base_url('assets/img/login.jpg')?>" />
-				</div>
-		       				
-                <div class="top col-left col-md-11" />
-				 <h1>Make it in time using Tempus</h1>
-                    <a href="<?php echo base_url('registration'); ?>" class="btton btn btn-info" role="button">SIGN UP NOW FOR FREE!</a>
+                </div>
+                <div class="col-md-8 homepageHead">                
+				 <h1>Make it in time using 
+				 	<br /><strong>Tempus</strong></h1>
+                    <a href="<?php echo base_url('registration'); ?>" class="btton btn btn-info" role="button">SIGN UP NOW!</a>
+                </div>
+                <div class="col-md-3">
                 </div>
             </div>
         </div>
 	</div>
     
 
-        <div class="section">
+        <div class="full-page">
             <div class=" container">
-				<div class="row">
-                    <div class="col-md-3">
-                        <h2>Never ask "what time is my meeting?" again</h2>
-                        <p>Clients can use our built-in calendar where you can see your real-time calendar availability and you can also put some notes on the date you like.</p>
+				<div class="row section2">
+                    <div class="col-md-5">
+                        <h2>Small things matter</h2>
+                        <p>Clients can use our built-in calendar where you can see your real-time calendar availability</p>
                     </div>
-                    <div class="col-md-9">
+                    <div class="col-md-7">
                         <img id="kalendaryo" src="<?php echo base_url('assets/img/ps1.png')?>" alt="calendar" width="100%" height="100%">
                     </div>
                 </div>
             </div>
          </div>
 		
-    <div class="section">
-        <div class="container container-fluid">
-		<div class="row">
-			<div class="col-md-9">
-			<img id="control" src="<?php echo base_url('assets/img/control.jpg')?>" alt="control" width="100%" height="100%">
-           
-        </div>    
-        <div class="col-md-3">
-			 <h2>Be in full control of your staffs' availability</h2>
-			<p>Manage and monitor your staffs' working hours.</p>
+    <div class="full-page">
+        <div class="container">
+		<div class="row section3">
+			<div class="col-md-8">
+				<img id="control" src="<?php echo base_url('assets/img/device2.jpg')?>" alt="control" width="100%" height="100%">
+        	</div>    
+        	<div class="col-md-4">
+			 	<h2>Take it all with you</h2>
+				<p>Switch between devices, and pick up wherever you left off.</p>
+			</div>
 		</div>
 		</div>
-	</div>
 	</div>
 	
 		
-	<div class="section">
-		<div class="container">
-			
-				<div class="align-center">
-				<h2>How it works.</h2>
-				</div>
-				<div class="row">
+	<div class="full-page">
+		<div class="container ">
+				<div class="row perks">
 					<div class="align-center col-md-4">
 						<img id="bilog" src="<?php echo base_url('assets/img/icon1.png')?>">
 						<h3>Suggest Options</h3>
@@ -116,78 +85,32 @@
 						<h3>Monitor staff services</h3>
 						<p>You can input duration, description, price and service provider in that specific kind of service.</p>
 					</div>
-				
 			</div>
 		</div>
-		
+	</div>
+				<div class="footer">
+					Tempus Multi-Tenant System | <i class="fa fa-fw fa-facebook-official"></i><a href="#">tempuscompany</a> | <i class="fa fa-fw fa-envelope-square"></i><a href="#">tempuscompany@gmail.com</a>
+				</div>	
 
-	<div class="container">
-	<div class="bg21 row">	
-		<div class="align-center">
-			<h4>FOR INQUIRIES</h4>
-		</div>
-			<div class="row">
-			<div class="align-center col-md-4">
-				<div class="align-center col-md-1">
-				<img id="logoo" class="fuck1"src="<?php echo base_url('assets/img/phone.png')?>">
-				<div>
-				<img id="logoo" class="fuck "src="<?php echo base_url('assets/img/fb.png')?>">
-				</div>
-				</div>
-				<p>09111111111 / 0922222222</p>
-				<p>facebook.com/TempusCompany</p>
-			</div>
-			<div class="align-center col-md-4">
-				<div class="align-center col-md-1">
-				<img id="logoo2" src="<?php echo base_url('assets/img/ig.png')?>">
-				<div>
-				<img id="logoo2" src="<?php echo base_url('assets/img/twtr.png')?>">
-				</div>
-				</div>
-				<a>@TempusCompanyOfficial</a>
-				<a>@TempusCompanyOfficial</a>
-			</div>
-			<div class="align-center col-md-4">
-				<div class="align-center col-md-1">
-				<img id="logoo2" src="<?php echo base_url('assets/img/email.png')?>">
-				</div>
-				<a>tempuscompany@gmail.com</a>
-			</div>
-		</div>
-	</div>
-</div>			
+
 </div>	
-	<div class="row bottom-section">
-		<div class="bottom-content">
-			<p><strong>Tempus</strong>Multi Tenant System</p>
-		</div>
-	</div>
+
  
 	 
   
 
 
 
-
- <script>
-       $(document).ready(function() {
-         $('#wrapper').fullpage({
-         /*       autoScrolling: false*/
-            });
-       });
-   </script>>
-   
-   
 <script>
-// Get the modal
-var modal = document.getElementById('id01');
+    vph = $(window).height();
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+    $('.full-page').height(vph);
+
+/*
+    $(window).resize(function () {
+    $('.full-page').css('height', $(window).height() * 0.8);
+});
+$(function(){ $(window).resize() });*/
 </script>
 
 </body>

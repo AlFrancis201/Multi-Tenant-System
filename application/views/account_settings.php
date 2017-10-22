4fa482282c3e74132b0fe8dc7c73b34a894d2636
Leonardo Pajuyo <pajuyo.leo@gmail.com>
2017-10-23 04:43:10 +0800

    added per staff calendar view

---
+++ application/views/account_settings.php
@@ -0,0 +1,18 @@
+<div class="content-wrapper">
+    <section class="content-header">
+      <h1>
+        Account Settings
+        <small>Control panel</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="<?php echo base_url('mts'); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Account Settings</li>
+      </ol>
+    </section>
+    
+    <section class="content">
+        <!-- form here -->
+    </section>
+</div>
+</body>
+</html>

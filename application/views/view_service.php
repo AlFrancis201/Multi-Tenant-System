723c13bbef6d37dd1072ef48184c62771cc16ab5
DaleAngeles <dale.angeles09@gmail.com>
2017-10-22 17:08:03 +0800

    sidebarv2

--- application/views/view_service.php
+++ application/views/view_service.php
@@ -9,22 +9,36 @@
         <li class="active">Services</li>
       </ol>
     </section>
-    <section class="sidebar sidebar2">
-           <a href="<?php echo base_url('mts/add_service'); ?>"><button>Add Service</button></a>
-          <!-- Sidebar user panel -->
-          <ul class="sidebar-menu sad" data-widget="tree">
-            <li class="header2">STAFF LIST</li>
-                        <?php
-                            if($service_record != false){
-                                foreach($service_record as $s){
-                                    echo '<li><a class="nav-link" href="'.base_url('mts/view_service_profile/'.$s['service_id']).'">'.$s['service_name'].'</a></li>';
-                                }
-                            }
-                        ?>
-          </ul>
-    </section>  
-
-
+    <section class="content">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="box box-solid box-default sidebar2">
+                <ul class="sidebar-menu sad" data-widget="tree">
+                  <li class="header2">SERVICE LIST </li>
+                              <?php
+                                  if($service_record != false){
+                                      foreach($service_record as $s){
+                                          echo '<li><a class="nav-link" href="'.base_url('mts/view_service_profile/'.$s['service_id']).'">'.$s['service_name'].'</a></li>';
+                                      }
+                                  }
+                              ?>
+                </ul>
+          </div> 
+           <a href="<?php echo base_url('mts/add_service'); ?>"><button class="btn" id="serviceAdd">Add Service</button></a> 
+        </div>
+        <div class="col-md-9">
+          <div class="box box-solid box-default">
+            <ul class=" sidebar-menu sad">
+              <li class="header2"> CONTENT </li>
+            </ul>
+            <div class="container">
+                  CONTENT
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 
 </div>
 </body>
